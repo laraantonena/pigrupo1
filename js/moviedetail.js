898745e6792afaf561d6accac1eac092e442edfb
renadaffara <rdaffara@udesa.edu.ar>
2025-06-05 11:18:30 -0300

    arregle todos los js de movie y de serie y el html

--- js/moviedetail.js
+++ js/moviedetail.js
@@ -1,10 +1,10 @@
 const apiKey = "c251b061cc3873b5dfe2bf2ae9caae5e"; 
-const movieid = ""; 
 const movieDetailUrl = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&language=es-ES`;
 
 let queryString = location.search;
 let queryStringObj = new URLSearchParams(queryString);
 let id = queryStringObj.get("id");
+let section = ``; 
 
 fetch()
 then(function(response){
@@ -12,15 +12,22 @@ then(function(response){
 })
     .then(function(data) {
       
-        console.log(data);
-        document.querySelector(".namepj").innerText = data.title;
-        document.querySelector(".releasedatepj").innerText = `Fecha de Estreno: ${data.release_date}`;
-        document.querySelector(".genrepj").innerText = `Género: ${data.genres[0].name}`;
-        document.querySelector(".ratingpj").innerText = `Calificación: ${data.vote_average}`;
-        document.querySelector(".runtimepj").innerText = `Duración: ${data.runtime} minutos`;
-        document.querySelector(".overviewpj").innerText = `Sinopsis: ${data.overview}`;
-        document.querySelector(".imgpj").src = `${data.image}`;
+          let section= data.results;
+    
+     for(let i = 0; i < section.length; i++) {
+        section += ` 
+        <article id="detail-movie">
+                <h1 class="namee"> Nombre de la pelicula: </h1>
+                <img class="imgn" src=""  alt="imagen de pelicula">
+                <h4 class="releasedatee"> Fecha de lanzamiento: </h4>
+                <h4 class="genrepj"> Genero de la pelicula: </h4>
+                <h4 class="ratingpj"> Calificacion: </h4>
+                <h4 class="runtimepj"> Tiempo de duracion: </h4>
+                <p class="decripcionpj"> Descripcion: </p>
+         </article>
+        `}
     })
+
    .catch(function(error){
     console.error("Error: " + error)
 })
