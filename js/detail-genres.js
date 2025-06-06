c55ac7f5a3f6f54f537273fe6908f0a2803b488a
nicdocto <ndoctorovich@udesa.edu.ar>
2025-06-06 11:34:32 -0300

    css

--- js/detail-genres.js
+++ js/detail-genres.js
@@ -7,17 +7,22 @@ const genreId = queryParams.get('id');
 const type = queryParams.get('type'); 
 const genreName = queryParams.get('name');
 
+const section = document.querySelector('section');
+section.classList.add('detailgenres');
+
 document.querySelector('h1').innerText = `Género: ${genreName}`;
 
 const endpoint = `https://api.themoviedb.org/3/discover/${type}?api_key=${apiKey}&language=es-ES&with_genres=${genreId}`;
 
 fetch(endpoint)
-  .then(res => res.json())
-  .then(data => {
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
     const section = document.querySelector('section');
     let html = '';
 
-    data.results.slice(0, 10).forEach(item => {
+    data.results.slice(0, 10).forEach(function(item) {
       const title = item.title || item.name;
       const imgSrc = item.poster_path
         ? `https://image.tmdb.org/t/p/w500${item.poster_path}`
