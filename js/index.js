93e05f23fae11f98013eb7500223b203a6848047
unknown <lantonenaposse@udesa.edu.ar>
2025-06-05 23:13:24 -0300

    index terminado

--- js/index.js
+++ js/index.js
@@ -2,16 +2,19 @@ const apiKey = "c251b061cc3873b5dfe2bf2ae9caae5e";
 
 const moviepopularsurl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`
 const tvpopularsurl = `https://api.themoviedb.org/3/tv/popular?api_key=${apiKey}`;
+const criticaurl = `https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}`
 // const moviePopularUrl = `https://api.themoviedb.org/3/movie/popular?api_key=c251b061cc3873b5dfe2bf2ae9caae5e`;
 // const tvPopularUrl = `https://api.themoviedb.org/3/tv/popular?api_key=c251b061cc3873b5dfe2bf2ae9caae5e`;
 
 const movieContainer = document.querySelector('.peliculaspopulares');
 const seriesContainer = document.querySelector('.seriespopulares');
+const criticaContainer = document.querySelector('.criticapeliculas');
 
 let moviesHTML = '';
 let seriesHTML = '';
+let criticaHTML = '';
 
-// Fetch popular movies
+
 fetch(moviepopularsurl)
 .then(function(response){
     return response.json();
@@ -26,7 +29,7 @@ fetch(moviepopularsurl)
                 <img src="https://image.tmdb.org/t/p/w500/${movies[i].poster_path}"  alt="imagen de pelicula">
                 </a>
                 <h3>${movies[i].title}</h3>
-                <p>${movies[i].overview}</p>
+                
             </article>
         `;
     }
@@ -35,6 +38,31 @@ fetch(moviepopularsurl)
 .catch(function(error){
     console.error("Error: " + error)
 })
+
+fetch(criticaurl)
+.then(function(response){
+    return response.json();
+})  
+.then(function(data){
+    let criticas = data.results;
+
+    for(let i = 0; i < criticas.length; i++) {
+        criticaHTML += `
+            <article class="foto">
+                <a href="./detail-movie.html">
+                <img src="https://image.tmdb.org/t/p/w500/${criticas[i].poster_path}" alt="imagen de pelicula">
+                </a>
+                <h3>${criticas[i].title}</h3>
+                
+            </article>
+        `;
+    }
+    criticaContainer.innerHTML = criticaHTML;
+})
+.catch(function(error){
+    console.error("Error: " + error)
+})
+
 
 fetch(tvpopularsurl)
 .then(function(response){
@@ -50,7 +78,7 @@ fetch(tvpopularsurl)
                 <img src="https://image.tmdb.org/t/p/w500/${series[i].poster_path}" alt="imagen de serie">
                 </a>
                 <h3>${series[i].name}</h3>
-                <p>${series[i].overview}</p>
+                
             </article>
         `;
     }
